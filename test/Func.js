eb9a34937e10ee953bb8c8fa8236e8046d8e0237
Alan R. Soares <alanrsoars@gmail.com>
2015-08-14 01:13:39 +1200

    add tests for Func.apply and Func.flip

--- test/Func.js
+++ test/Func.js
@@ -11,4 +11,24 @@ describe('Func.js', () => {
       expect(increment(1)).to.equal(2);
     });
   });
+
+  describe('Funct.apply', () => {
+    it('Should return the application of the supplied list as arguments to the supplied function', () => {
+      const sum = Func.curry((a, b) => a + b);
+      const sumAll = (...xs) => xs.reduce(sum);
+      expect(Func.apply(sum, [2, 3])).to.equal(5);
+      expect(Func.apply(sumAll, [1, 2, 3, 4, 5])).to.equal(15);
+    });
+  });
+
+  describe('Funct.flip', () => {
+    it('Should return a function with the arguments flipped', () => {
+      const subtract = (a, b) => a - b;
+      const invertedSubtract = Func.flip(subtract);
+      const invertedPower = Func.flip(Math.pow);
+      expect(subtract(2, 5)).to.equal(-3);
+      expect(invertedSubtract(2, 5)).to.equal(3);
+      expect(invertedPower(2, 5)).to.equal(25);
+    });
+  });
 });
