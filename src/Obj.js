068f85b3fa11f9ae4f84f001f0ea475176195506
Alan R. Soares <alanrsoars@gmail.com>
2015-08-23 21:34:18 +1200

    new tests for Obj module

--- src/Obj.js
+++ src/Obj.js
@@ -10,12 +10,11 @@ export const values = (x) => keys(x).map((k) => x[k]);
 export const clone = (x) => JSON.parse(JSON.stringify(x));
 
 export const reduce = curry((fn, initial, x) =>
-  Object.keys(x)
-        .reduce((memo, k, i) => fn(memo, k, x[k], i), initial));
+  keys(x).reduce((memo, k, i) => fn(memo, k, x[k], i), initial));
 
 //:: (b → c) → {a: b} → {a: c}
 export const map = curry((fn, x) =>
-  x.keys().map((k, i) => fn(k, x[k], i)));
+  keys(x).map((k, i) => fn(k, x[k], i)));
 
 //:: {a: b} -> {a: b} -> {a: b}
 export const merge = (y, ...xs) =>
--- test/Obj.js
+++ test/Obj.js
@@ -12,4 +12,17 @@ describe('Obj.js', () => {
       expect(Obj.values({ a: 2, b: 3, c: 9 })).to.deep.equal([2, 3, 9]);
     });
   });
+  describe('Obj.clone', () => {
+    it('Should return a copy of a given object, with no reference to it', () => {
+      expect(Obj.clone({ a: 2, b: 3, c: 9 })).to.deep.equal({ a: 2, b: 3, c: 9 });
+      expect(Obj.clone({ a: 2, b: 3, c: 9 })).not.to.equal({ a: 2, b: 3, c: 9 });
+    });
+  });
+  describe('Obj.merge', () => {
+    it('Should merge any number of objects to the first given object', () => {
+      expect(Obj.merge({ a: 2, c: 9 }, { b: 3 })).to.deep.equal({ a: 2, b: 3, c: 9 });
+      expect(Obj.merge({}, { a: 2, c: 9 }, { b: 3 })).to.deep.equal({ a: 2, b: 3, c: 9 });
+      expect(Obj.merge({ a: 2, c: 9 }, { b: 3 }, { a: 4 })).to.deep.equal({ a: 4, b: 3, c: 9 });
+    });
+  });
 });
