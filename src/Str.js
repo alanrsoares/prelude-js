0faf26fa983ede224a3136974e47f41a4edda5a8
Alan R. Soares <alanrsoars@gmail.com>
2015-08-25 00:49:13 +1200

    add function Str.camelize

--- src/Str.js
+++ src/Str.js
@@ -1,4 +1,10 @@
+//:: String → String
 export const capitalize = (x) => x.charAt(0).toUpperCase() + x.slice(1);
 
+//:: String → String
 export const dasherize = (x) =>
   x.replace(/[A-Z]/g, (m) => `-${ m.toLowerCase() }`);
+
+//:: String → String
+export const camelize = (x) =>
+  x.replace(/-(\w)/g, (m) => m[1].toUpperCase());
