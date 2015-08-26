f12edda268989b1801f348e020f3d1d458826ac3
Alan R. Soares <alanrsoars@gmail.com>
2015-08-27 01:45:13 +1200

    add module builder scripts

---
+++ scripts/build-index.js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import minimist from 'minimist';
+
+const ARGS = minimist(process.argv.slice(2));
+const MODULE_PATH = `./src/${ARGS.module}`;
+const fns = fs.readdirSync(MODULE_PATH)
+                .map((i) => i.replace(/(.+)\.js$/, '$1'));
+const imports = fns.map((fn) => `import ${fn} from './${fn}'` ).join(';\n');
+const file =
+`${ imports };\n
+export default {
+  ${ fns.join(',\n  ') }
+};\n`;
+
+console.log(file);
