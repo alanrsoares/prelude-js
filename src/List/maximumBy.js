f047885f8360d1fba1e94d388af0e843c3daa4b6
Alan R. Soares <alanrsoars@gmail.com>
2015-09-17 23:29:43 +1200

    fix List.minimumBy and List.maximumBy

--- src/List/maximumBy.js
+++ src/List/maximumBy.js
@@ -1,4 +1,4 @@
 import curry from '../Func/curry';
 
 //+ maximunBy :: (a -> b) -> [a] -> b
-export default curry((fn, xs) => xs.reduceRight((max, x) => fn(x) > max ? x : max));
+export default curry((fn, xs) => xs.reduceRight((max, x) => fn(x) > fn(max) ? x : max));
--- src/List/minimumBy.js
+++ src/List/minimumBy.js
@@ -1,4 +1,4 @@
 import curry from '../Func/curry';
 
 //+ minimumBy :: (a -> b) -> [a] -> b
-export default curry((fn, xs) => xs.reduceRight((min, x) => fn(x) < min ? x : min));
+export default curry((fn, xs) => xs.reduceRight((min, x) => fn(x) < fn(min) ? x : min));
