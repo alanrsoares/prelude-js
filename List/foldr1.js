55786f56e918bd2c92e102d6ff66def5b9c480e7
Alan R. Soares <alanrsoars@gmail.com>
2015-09-23 15:46:19 +1200

    build dist

--- List/foldr1.js
+++ List/foldr1.js
@@ -14,7 +14,15 @@ var _foldr = require('./foldr');
 
 var _foldr2 = _interopRequireDefault(_foldr);
 
+var _last = require('./last');
+
+var _last2 = _interopRequireDefault(_last);
+
+var _initial = require('./initial');
+
+var _initial2 = _interopRequireDefault(_initial);
+
 exports['default'] = (0, _FuncCurry2['default'])(function (fn, xs) {
-  return (0, _foldr2['default'])(fn, 0, xs);
+  return (0, _foldr2['default'])(fn, (0, _last2['default'])(xs), (0, _initial2['default'])(xs));
 });
 module.exports = exports['default'];
