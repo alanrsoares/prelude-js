08c72b902a3ffb17c74668aacdbe3684a530512f
Alan R. Soares <alanrsoars@gmail.com>
2015-09-23 15:45:57 +1200

    fix fold and foldr1

--- src/List/foldl1.js
+++ src/List/foldl1.js
@@ -1,4 +1,7 @@
 import curry from '../Func/curry';
+import fold from './fold';
+import head from './head';
+import tail from './tail';
 
 //+ foldl1 :: (a -> a -> a) -> [a] -> a
-export default curry((fn, xs) => xs.reduce(fn, 0));
+export default curry((fn, xs) => fold(fn, head(xs), tail(xs)));
--- src/List/foldr1.js
+++ src/List/foldr1.js
@@ -1,5 +1,7 @@
 import curry from '../Func/curry';
 import foldr from './foldr';
+import last from './last';
+import initial from './initial';
 
 //+ foldr1 :: (a -> a -> a) -> [a] -> a
-export default curry((fn, xs) => foldr(fn, 0, xs));
+export default curry((fn, xs) => foldr(fn, last(xs), initial(xs)));
