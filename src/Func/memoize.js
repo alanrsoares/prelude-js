0bbd66d993f987022aa707097472c236659dc348
Alan R. Soares <alanrsoars@gmail.com>
2015-09-25 11:27:54 +1200

    Use spread operator

--- src/Func/memoize.js
+++ src/Func/memoize.js
@@ -1,8 +1,8 @@
 //+ memoize :: (a -> b) -> a -> b
 export default (fn) => {
-  let memo = {};
+  const memo = {};
   return (...args) => {
     const key = args.map((arg) => arg + typeof arg).join('');
-    return (key in memo) ? memo[key] : (memo[key] = fn.apply(null, args));
+    return (key in memo) ? memo[key] : (memo[key] = fn(...args));
   };
 };
