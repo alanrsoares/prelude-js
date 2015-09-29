3aceb123a33424802f2fb9b5c0212fbe1d645cc7
Alan R. Soares <alanrsoars@gmail.com>
2015-09-29 16:03:50 +1300

    add docs builder script

---
+++ scripts/build-readme.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+const moduleNames = fs.readdirSync('./src')
+                      .filter((fn) => fn !== 'index.js');
+
+const low = (x) => x.toLowerCase();
+const up = (x) => x.toUpperCase();
+
+function functions(m) {
+  return fs.readdirSync(`./src/${ m }`)
+           .map((i) => i.replace(/(.+)\.js$/, '$1'))
+           .filter((fn) => fn !== 'index')
+           .map((f) => `\n\t* [${ low(f) }](${ up(m) }.md#${ low(m + f) })`);
+}
+
+const modules = moduleNames.map((m) => `\n* [${ m }](${ m }.md)${ functions(m) }`);
+
+const file =
+`# Preλude-js :: Docs\n
+## Modules\n
+${ modules.join('') }`;
+
+console.log(file);
