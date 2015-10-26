a104f64163e20d3d05fea75dd586aca185063850
Alan R. Soares <alanrsoars@gmail.com>
2015-10-27 11:05:07 +1300

    add new function Str.padLeft

---
+++ Str/padLeft.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _FuncCurry = require('../Func/curry');
+
+var _FuncCurry2 = _interopRequireDefault(_FuncCurry);
+
+exports['default'] = (0, _FuncCurry2['default'])(function (p, s) {
+  return p.substring(0, p.length - s.toString().length) + s;
+});
+module.exports = exports['default'];
