9e454453391a956668b5636b78821517edadde03
Alan R. Soares <alanrsoars@gmail.com>
2015-12-02 17:25:53 +1300

    add cases micro library

---
+++ test/cases.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze'
+import { curry } from '../src/Func';
+
+const shallow = (actual, expected) => expect(actual).to.equal(expected);
+
+const deep = (actual, expected) => expect(actual).to.deep.equal(expected);
+
+const runCase = curry((f, { input, expected, comparison }) => {
+  deepFreeze(input);
+  deepFreeze(f);
+  comparison(f(...input), expected);
+});
+
+export const Case =
+  (input, expected, comparison = deep) =>
+    ({ input, expected, comparison });
+
+export const run =
+  (f, ...cases) =>
+    cases.map(runCase(f));
