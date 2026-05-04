ab3f6f9f01c96f4de88cc17e6b391fda526d1536
Alan R. Soares <alanrsoars@gmail.com>
2026-05-05 10:08:25 +1200

    fix(docs): make docs generation deterministic

--- scripts/build-readme.js
+++ scripts/build-readme.js
@@ -1,24 +1,29 @@
-import { readdirSync } from "fs";
+import { readdirSync } from 'fs'
 
-const moduleNames = readdirSync("./src").filter((fn) => fn !== "index.js");
+const byName = (a, b) => a.localeCompare(b)
 
-const low = (x) => x.toLowerCase();
-const up = (x) => x.toUpperCase();
+const moduleNames = readdirSync('./src')
+  .filter((fn) => fn !== 'index.js')
+  .sort(byName)
+
+const low = (x) => x.toLowerCase()
+const up = (x) => x.toUpperCase()
 
 function functions(m) {
   return readdirSync(`./src/${m}`)
     .filter((f) => !/index/.test(f))
-    .map((f) => f.replace(/(.+)\.js$/, "$1"))
+    .sort(byName)
+    .map((f) => f.replace(/(.+)\.js$/, '$1'))
     .map((f) => `\n\t* [${low(f)}](${up(m)}.md#${low(m + f)})`)
-    .join("");
+    .join('')
 }
 
 const modules = moduleNames.map(
   (m) => `\n* [${m}](${up(m)}.md)${functions(m)}`
-);
+)
 
 const file = `# Preλude-js :: Docs\n
 ## Modules\n
-${modules.join("")}`;
+${modules.join('')}\n`
 
-console.log(file);
+console.log(file)
